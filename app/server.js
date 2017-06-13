ec058be33521630822d5d33cde93f4f8d60b6b80
sanattaori <sanattaori3@gmail.com>
2017-06-13 17:13:38 +0530

    email verify fix

--- app/server.js
+++ app/server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var request = require('request');
 var app = express()
 app.use( bodyParser.json() )
 
@@ -22,11 +23,29 @@ app.post('/test-page', function(req, res) {
     
 });
 
-app.get('/verify-email/:token',function(req,res){
-token = req.params.token;
+app.get('/verify-email/:toke',function(req,res){
 
-res.cookie('name', token, {maxAge : 36000000});
-res.redirect('/app');
+var toke = req.params.toke;
+
+request('https://auth.project.sanattaori.me/email/confirm?token='+ toke, function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred 
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+  console.log('body:', body); // Print the HTML for the Google homepage. 
+  if ((response && response.statusCode)===	200) {
+res.send('User verified Login to continue');
+
+
+}
+else if ((response && response.statusCode)===400) {
+
+res.send('Invalid User Or Expired Key');
+
+}
+});
+
+
+
+
 });
 
 app.get('/app', function (req, res) {
